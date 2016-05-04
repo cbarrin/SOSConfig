47a6cfd1c882e7ac56cb63971cc2aaf3b47a5082
Geddings Barrineau <cbarrin@g.clemson.edu>
2016-05-04 14:10:17 -0400

    Adding main configuration script template with comments. We will fill this in as we go.

---
+++ ConfigureSOS.py
@@ -0,0 +1,21 @@
+# This is the main configuration file for setting up SOS.
+# It will be responsible for doing a few things:
+#       - Deleting firewall rules
+#       - Deleting any queueing system
+#       - Configuring all the network parameters
+#       - Pinning all of the interrupts
+#       - Installing and configuring OVS
+#       - Installing and configure the SOS agent
+
+
+# STEP 1: Deleting firewall rules
+
+# STEP 2: Deleting queueing systems
+
+# STEP 3: Configure any network parameters
+
+# STEP 4: Pin any interrupts to core 0
+
+# STEP 5: Install and configure OVS
+
+# STEP 6: Install and configure SOS agent
