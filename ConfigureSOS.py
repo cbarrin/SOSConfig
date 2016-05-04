fba4f71ec92c98a284032ec9f9820fe1f82c1f18
jzulfiq <jzulfiq@agent-1.3agents.strd-of-srv-pg0.clemson.cloudlab.us>
2016-05-04 17:12:35 -0400

    Added check for Queues

--- ConfigureSOS.py
+++ ConfigureSOS.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # This is the main configuration file for setting up SOS.
 # It will be responsible for doing a few things:
 #       - Deleting firewall rules
@@ -11,10 +12,17 @@ import subprocess
 # STEP 1: Deleting firewall rules
 
 # STEP 2: Deleting queueing systems
+##We might have to extend this to iterate for every interface
 
 interface = raw_input("Which interface to delete the queues?? >>")
-subprocess.call("sudo tc qdisc del dev " + interface + " root", shell=True)
-subprocess.call("sudo tc -s qdisc ls dev " + interface, shell=True)
+print "\nDeleting queues for interface",interface
+ret = subprocess.check_output("sudo tc -s qdisc ls dev " + interface, shell=True)
+if ret:
+  subprocess.call("sudo tc qdisc del dev " + interface + " root", shell=True)
+  #subprocess.call("sudo tc -s qdisc ls dev " + interface, shell=True)
+  print "Queues deleted for interface",interface
+else:
+  print "No queues found for interface",interface
 
 # STEP 3: Configure any network parameters
 
